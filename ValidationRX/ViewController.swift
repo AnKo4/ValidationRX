ed25b5ce5222d09a2474ed51bdb6c34ca9063755
msoft <msoft@mac-mini-msoft.local>
2019-07-10 13:48:15 +0400

    Initial Commit

---
+++ ValidationRX/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ValidationRX
+//
+//  Created by msoft on 10.07.2019.
+//  Copyright © 2019 Mediasoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
